cf140b57b797cae6ee7e17a2061ca74d4df28580
linteng <875482941@qq.com>
2020-05-07 16:07:13 +0800

    update README.md && add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  parser: "babel-eslint",
+  ignorePatterns: ["config", "dist", "cdnConfig", "webpack", "node_modules/"],
+  rules: {
+    "linebreak-style": [0 ,"error", "windows"],
+    "global-require": 0,
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-membe": 0,
+    "import/no-dynamic-require": 0,
+    "import/no-unresolved": 0,              // 允许import不在node_modules里的模块
+    "react/jsx-props-no-spreading": 0,      // 允许this.props
+    "react/destructuring-assignment": 0,    // 允许this.state
+    "no-console":"off",                      // 允许打印
+    "no-param-reassign": 0,                 // 允许修改传入的参数
+    "import/extensions": 0,
+    "react/prop-types": 0,  // 允许使用children
+    "no-nested-ternary": 0,                  // 三元判断
+    "no-restricted-syntax": 0,              // 允许使用for in, for of
+    "func-names": 0,                         // 允许不给function name
+    "no-return-assign":0,                    // 允许在返回语句中赋值
+    "no-plusplus": 0,                        // 允许使用++
+    "no-prototype-builtins": 0,               // 允许Object.prototype上的属性
+    "strict": 0,                              // 允许strict
+    "lines-around-directive": 0,
+    "jsx-a11y/click-events-have-key-events": 0, // 允许点击事件不需绑定键盘事件
+    "jsx-a11y/no-static-element-interactions": 0,    // 允许
+    "prefer-destructuring": 0,                  // 允许不优先使用数组和对象解构
+    "jsx-a11y/anchor-has-content": 0,            // 允许在闭合标签不写内容
+    "jsx-a11y/control-has-associated-label": 0,
+    "jsx-a11y/anchor-is-valid": 0,
+    "no-script-url": 0,
+    "react/self-closing-comp": 0,
+    "jsx-a11y/label-has-associated-control": 0,
+    "import/prefer-default-export": 0,           // 允许没有默认导出
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],     // 末尾允许并强制空1行
+    // "object-curly-newline": ["error", "always"],
+    // "react/jsx-indent": 2,
+    "no-underscore-dangle": 0
+   },
+};
--- libs/crypto.js
+++ libs/crypto.js
@@ -11,20 +11,21 @@ const fs = require('fs');
 const path = require('path')
 
 let NodeRSA = require('node-rsa');
-const MAX_DECRYPT_BLOCK = 128 		//RSA最大解密密文大小
+const MAX_DECRYPT_BLOCK = 128 //RSA最大解密密文大小
 
 class RSA {
-	
-	constructor(c){
 
+	constructor(pubkey, prikey) {
+		/* pubkey && (PUBLIC_KEY = pubkey)
+		prikey && (PRIVATE_KEY = prikey) */
 		/*this.RSA_PUBLIC_KEY = new NodeRSA({b: 1024}); 		//1024位加密
 		this.RSA_PUBLIC_KEY.setOptions({encryptionScheme: 'pkcs1'}) 		//RSA公开密钥算法加密和签名机制
 		this.RSA_PUBLIC_KEY.importKey(RSA.PUBLIC_KEY)*/
 	}
 
-	checkType(data){
+	checkType(data) {
 		let type = Object.prototype.toString.call(data)
-		switch(type){
+		switch (type) {
 			case "[object Array]":
 				return 1;
 				break;
@@ -43,8 +44,8 @@ class RSA {
 		}
 	}
 
-	rsa_encode(data, act = 'public'){
-		if(this.checkType(data) !== 2)
+	rsa_encode(data, act = 'public') {
+		if (this.checkType(data) !== 2)
 			data = {}
 
 		let RSA_KEY = new NodeRSA({b: 1024}); 		//1024位加密
@@ -57,13 +58,13 @@ class RSA {
 			data = RSA_KEY.encryptPrivate(JSON.stringify(data), 'base64')
 		}
 
-	     
+
 		return data
 
 	}
 
-	rsa_decode(str, act = 'private'){
-		
+	rsa_decode(str, act = 'private') {
+
 		let decryptedData = null
 		if(act === 'private'){
 			try{
@@ -79,33 +80,33 @@ class RSA {
 			}
 		}
 
-	    return decryptedData
+		return decryptedData
 
 	}
 
 
-	rsa_public_decode(str, act){
-	    let res = null
-			try {
-		    let encryptedBuffer = Buffer.from(str, 'base64');
-		    let decryptedBuffers = [];
+	rsa_public_decode(str, act) {
+		let res = null
+		try {
+			let encryptedBuffer = Buffer.from(str, 'base64');
+			let decryptedBuffers = [];
 
-		    let totalBuffers = encryptedBuffer.length / MAX_DECRYPT_BLOCK;
-		    for (let i = 0; i < totalBuffers; i++) {
-		        let tempBuffer = new Buffer(MAX_DECRYPT_BLOCK);
-		        encryptedBuffer.copy(tempBuffer, 0, i * MAX_DECRYPT_BLOCK, (i + 1) * MAX_DECRYPT_BLOCK);
-		        let decryptedBuffer = this.rsa_decode(tempBuffer, act)
-		        decryptedBuffers.push(decryptedBuffer)
-		    }
-		    
-				res = Buffer.concat(decryptedBuffers).toString('utf8')
-	    } catch (e) {
-				return false
-	    }
-	    return res
+			let totalBuffers = encryptedBuffer.length / MAX_DECRYPT_BLOCK;
+			for (let i = 0; i < totalBuffers; i++) {
+				let tempBuffer = new Buffer(MAX_DECRYPT_BLOCK);
+				encryptedBuffer.copy(tempBuffer, 0, i * MAX_DECRYPT_BLOCK, (i + 1) * MAX_DECRYPT_BLOCK);
+				let decryptedBuffer = this.rsa_decode(tempBuffer, act)
+				decryptedBuffers.push(decryptedBuffer)
+			}
+
+			res = Buffer.concat(decryptedBuffers).toString('utf8')
+		} catch (e) {
+			return false
+		}
+		return res
 	}
 
-	create_sign(data){
+	create_sign(data) {
 
     var sign = crypto.createSign('RSA-SHA1');
     if (Object.prototype.toString.call(data) !== '[object String]')
@@ -127,58 +128,58 @@ class RSA {
 	    return verify.verify(RSA.PUBLIC_KEY, signature)
 	}
 
-	cipheriv_encode(str, key, iv = null){
+	cipheriv_encode(str, key, iv = null) {
 
-	    var iv = iv ? iv : libs.SEC.rand(16);
+		var iv = iv ? iv : libs.SEC.rand(16);
 
-	    const cipherChunks = [];   
-	    const ciper = crypto.createCipheriv('aes-128-cbc', key, iv);  
-	    cipherChunks.push(ciper.update(str, 'utf-8', 'hex'));    
-	    cipherChunks.push(ciper.final('hex'));   
-	    return iv + cipherChunks.join('') + key
+		const cipherChunks = [];
+		const ciper = crypto.createCipheriv('aes-128-cbc', key, iv);
+		cipherChunks.push(ciper.update(str, 'utf-8', 'hex'));
+		cipherChunks.push(ciper.final('hex'));
+		return iv + cipherChunks.join('') + key
 
 	}
 
 	cipheriv_decode(str) {
-	    var iv = str.slice(0, 16)
-	    var key = str.slice(str.length - 16, str.length)
-	    var data = str.slice(16, -16)
+		var iv = str.slice(0, 16)
+		var key = str.slice(str.length - 16, str.length)
+		var data = str.slice(16, -16)
 
-	    const cipherChunks = [];
-	    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-	    cipherChunks.push(decipher.update(data, 'hex', 'utf-8'));
-	    cipherChunks.push(decipher.final('utf-8'));
-	    return cipherChunks.join('');
+		const cipherChunks = [];
+		const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+		cipherChunks.push(decipher.update(data, 'hex', 'utf-8'));
+		cipherChunks.push(decipher.final('utf-8'));
+		return cipherChunks.join('');
 	}
 
-	cipheriv_encode_bykey(str, key, iv = null){
+	cipheriv_encode_bykey(str, key, iv = null) {
 
-	    var iv = iv ? iv : libs.SEC.rand(16);
+		var iv = iv ? iv : libs.SEC.rand(16);
 
-	    const cipherChunks = [];   
-	    const ciper = crypto.createCipheriv('aes-128-cbc', key, iv);  
-	    cipherChunks.push(ciper.update(str, 'utf-8', 'hex'));    
-	    cipherChunks.push(ciper.final('hex'));   
-	    return iv + cipherChunks.join('')
+		const cipherChunks = [];
+		const ciper = crypto.createCipheriv('aes-128-cbc', key, iv);
+		cipherChunks.push(ciper.update(str, 'utf-8', 'hex'));
+		cipherChunks.push(ciper.final('hex'));
+		return iv + cipherChunks.join('')
 
 	}
 
 	cipheriv_decode_bykey(str, key) {
-	    var iv = str.slice(0, 16)
-	    var data = str.slice(16)
+		var iv = str.slice(0, 16)
+		var data = str.slice(16)
 
-	    const cipherChunks = [];
-	    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-	    cipherChunks.push(decipher.update(data, 'hex', 'utf-8'));
-	    cipherChunks.push(decipher.final('utf-8'));
-	    return cipherChunks.join('');
+		const cipherChunks = [];
+		const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+		cipherChunks.push(decipher.update(data, 'hex', 'utf-8'));
+		cipherChunks.push(decipher.final('utf-8'));
+		return cipherChunks.join('');
 	}
 }
 
-RSA.PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, `../config/rsa_public_key.pem`), {
+RSA.PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, `../config/crypto/rsa_public_key.pem`), {
 	encoding: 'utf-8'
 })
-RSA.PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, `../config/rsa_private_key.pem`), {
+RSA.PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, `../config/crypto/rsa_private_key.pem`), {
 	encoding: 'utf-8'
 })
 module.exports = RSA
