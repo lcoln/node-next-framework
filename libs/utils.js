453d8a5465e2197aedfdccbdfd4f280e6f625fcd
guhongjie <332321510@qq.com>
2020-05-08 17:37:04 +0800

    update

--- libs/utils.js
+++ libs/utils.js
@@ -95,7 +95,48 @@ function suffix(str) {
   return str.slice(str.lastIndexOf('.') + 1);
 }
 
-exports.isFunction = isFunction;
+/**
+ * [对象深拷贝]
+ */
+function deepClone (obj) {
+  var copy;
+ 
+  // Handle the 3 simple types, and null or undefined
+  if (null == obj || "object" != typeof obj) return obj;
+
+  // Handle Date
+  if (obj instanceof Date) {
+    copy = new Date();
+    copy.setTime(obj.getTime());
+    return copy;
+  }
+
+  // Handle Array
+  if (obj instanceof Array) {
+    copy = [];
+    for (var i = 0, len = obj.length; i < len; i++) {
+      copy[i] = deepClone(obj[i]);
+    }
+    return copy;
+  }
+
+  // Handle RegExp
+  if (obj instanceof RegExp) {
+    return new RegExp(obj)
+  }
+
+  // Handle Object
+  if (obj instanceof Object) {
+    copy = {};
+    for (var attr in obj) {
+      if (obj.hasOwnProperty(attr)) copy[attr] = deepClone(obj[attr]);
+    }
+    return copy;
+  }
+
+  throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
 module.exports = function (ctx) {
   return {
     resolve,
@@ -111,3 +152,6 @@ module.exports = function (ctx) {
     checkFields,
   };
 };
+
+module.exports.isFunction = isFunction;
+module.exports.deepClone = deepClone;
