27823643fed29b1f2a1dfde5b7def3700934756b
guhongjie <332321510@qq.com>
2020-05-20 15:55:52 +0800

    update

--- scripts/ssrUseableLoader.js
+++ scripts/ssrUseableLoader.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (source) {
-  return 'exports.use = exports.ref = function() {};\nexports.unuse = exports.unref = function() {};';
+  return 'exports.use = exports.ref = exports.unuse = exports.unref = function() {};';
 };
