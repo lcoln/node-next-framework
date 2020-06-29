7482eb3e8d4b7bab0fd5c15b91c68d5b2e62bc3f
linteng <875482941@qq.com>
2020-06-29 13:03:03 +0800

    merge dev update cors middleware

--- libs/middleware/cros.js
+++ libs/middleware/cros.js
@@ -3,28 +3,28 @@ module.exports = {
   async optionsHandler(ctx, next) {
     // options处理
     if (ctx.request.req.method === 'OPTIONS') {
-      Utils.cross(ctx.response, '*');
-      ctx.response.send(200);
+      Utils.cross(ctx.resp, '*');
+      ctx.resp.send(200);
       return;
     }
 
     await next();
   },
   async corsHandler(ctx, next) {
-    console.log('middleware cros', { env });
+    console.log('middleware cros', { env, ctx });
     // 白名单允许跨域处理
     if (env === 'development') {
-      Utils.cross(ctx.response, '*');
+      Utils.cross(ctx.resp, '*');
     } else if (env === 'production') {
       // const host = ctx.request.headers('host');
-      const origin = ctx.response.headers('origin');
+      const origin = ctx.resp.headers('origin');
       const cors = ctx.get('cors') || [];
       console.log('middleware cros', { origin, cors });
       if (cors.some((v) => v.indexOf(origin) > -1)) {
-        Utils.cross(ctx.response, origin);
+        Utils.cross(ctx.resp, origin);
       }
       // if (cors.includes(host)) {
-      //   Utils.cross(ctx.response, host);
+      //   Utils.cross(ctx.resp, host);
       // }
     }
     await next();
