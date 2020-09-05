66b862b3d175473479644f9c02fc328b3aad1758
linteng <875482941@qq.com>
2020-09-05 14:23:41 +0800

    完善全局log调用api

--- core/log/index.js
+++ core/log/index.js
@@ -1,24 +1,62 @@
-function createLogContext(result, params, level, { requestId, req, request }) {
+const errorKeys = ['code', 'message', 'name', 'stack'];
+function createLogContext(desc, result = {}, params, level, { requestId, req, request }) {
+  if (!Utils.isString(desc)) {
+    return {
+      code: 400,
+      data: desc,
+      type: 'ReferenceError',
+      msg: '日志缺少描述',
+    };
+  }
   params = params
     ? (Utils.isString(params) ? params : JSON.stringify(params))
     : (request.query ? JSON.stringify(request.query) : null);
+  let res = {};
+  if (result) {
+    // 如果是字符串则赋值给data
+    if (Utils.isString(result)) {
+      res.data = result;
+    } else {
+      // 如果是一个抛出异常, 则赋值给res.error, res.data赋值null
+      if (result instanceof Error) {
+        res.error = Object.getPrototypeOf(result);
+        res.data = null;
+      } else if (result.error && result.error instanceof Error) {
+        // 如果有error对象, 则赋值给res.error
+        res.error = Object.getPrototypeOf(result.error);
+      }
+      if (res.error) {
+        res.error = errorKeys.reduce((item, next) => {
+          item[next] = res.error[next];
+          return item;
+        }, {});
+      }
+      // 如果有data对象, 则赋值给res.data
+      if (result.data) {
+        res.data = result.data;
+      }
+      res = JSON.stringify(res);
+    }
+  }
 
-  result = result
-    ? (Utils.isString(result) ? result : JSON.stringify(result))
-    : null;
   const { method, headers } = req;
   const timestemp = Date.now();
+
   // console.log({headers})
   return {
-    level, // debug, error
-    apiUrl: `${headers.host}/${req.url}`,
-    method,
-    params,
-    result,
-    requestId,
-    userAgent: headers['user-agent'],
-    timestemp,
-    date: Utils.dateFormat(timestemp),
+    code: 200,
+    data: {
+      description: desc,
+      level, // info, error
+      apiUrl: `${headers.host}/${req.url}`,
+      method,
+      params,
+      result: res,
+      requestId,
+      userAgent: headers['user-agent'],
+      timestemp,
+      date: Utils.dateFormat(timestemp),
+    },
   };
 }
 
@@ -38,10 +76,22 @@ function Log(ctx) {
 
 /**
  * error日志
+ * 用法:
+ *  class M extends Controller {
+ *    test () {
+ *      try {
+ *         throw (global.ReferenceError(3333));
+ *       } catch (e) {
+ *         this.log.error('异常', e);
+ *       }
+ *    }
+ *  }
+ * @param {any} desc   日志描述
  * @param {any} result 输出结果
  * @param {any} params 请求参数, 如果是get默认会填上参数不需要传
- * @returns {Object}
+ * @returns {console.log}
  * {
+ *   description: '',
  *   level: 'error',
  *   apiUrl: 'localhost:3334//hotmall/test',
  *   method: 'GET',
@@ -53,11 +103,21 @@ function Log(ctx) {
  *   date: '2020-09-05 00:10:40'
  * }
  */
-Log.prototype.error = function (result, params) {
-  console.log(createLogContext(result, params, 'error', this.ctx));
+Log.prototype.error = function (desc, result, params) {
+  const logContext = createLogContext(desc, result, params, 'error', this.ctx);
+  if (logContext.code >= 400 && global[logContext.type]) {
+    throw (global[logContext.type](logContext.msg));
+  }
+  console.log(logContext.data);
+  return logContext;
 };
-Log.prototype.info = function (result, params) {
-  console.log(createLogContext(result, params, 'info', this.ctx));
+Log.prototype.info = function (desc, result, params) {
+  const logContext = createLogContext(desc, result, params, 'info', this.ctx);
+  if (logContext.code >= 400 && global[logContext.type]) {
+    throw (global[logContext.type](logContext.msg));
+  }
+  console.log(logContext.data);
+  return logContext;
 };
 
 module.exports = Log;
